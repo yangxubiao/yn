6664179de8994f311c33bf0e46570105b31f2dea
yangxubiao <45391983+yangxubiao@users.noreply.github.com>
2019-11-06 22:56:39 +0800

    Update user.js

--- web-server-yn/routes/user.js
+++ web-server-yn/routes/user.js
@@ -30,105 +30,139 @@ router.get("/ulogin",(req,res)=>{
         res.send({code:-1,data:"not User"});
       }
     })
- })
+ });
 
 
-router.get("/register",(req,res)=>{
+(function(){
+   router.get("/register",(req,res)=>{
     var user=JSON.parse(req.query.user);
     var uname=user.name;
     var upwd=user.pass;
     var Rdate=user.date;
     var Cmoney=parseInt(user.money);
-  pool.query("SELECT uid FROM  yn_user where uname = ?",[uname],(err,result)=>{
-    if(err) throw err;
-     if(result.length>0){
-         res.send({code:3,msg:"用户名已存在,请换您的大名或者其他名称"})
-     }else{
-      pool.query("INSERT INTO yn_user VALUES(?,?,?,?,?)",[null,uname,upwd,Rdate,Cmoney],(err,result)=>{
-        if(err)  throw err;
-          if(result.affectedRows>0){
-            pool.query("SELECT * FROM  yn_user WHERE uname=? ",[uname],(err,result)=>{
-              if(err) throw err;
-                if(result.length>0){
-                       res.send({code:1,data:result})
-                   }else{
-                       res.send({code:-1,msg:"系统查找失败,请关闭浏览器页面,或者清理浏览器内存,之后在重新注册"})
-                  }
-               })
+    new Promise((reslove,reject)=>{
+      pool.query("SELECT uid FROM  yn_user where uname = ?",[uname],(err,result)=>{
+        if(err) throw err;
+         if(result.length>0){
+               res.send({code:3,msg:"用户名已存在,请换您的大名或者其他名称"})
+            }else{
+              reslove();
+            }
+        })
+    }).then(async function(){
+      new Promise((reslove,reject)=>{
+        pool.query("INSERT INTO yn_user VALUES(?,?,?,?,?)",[null,uname,upwd,Rdate,Cmoney],(err,result)=>{
+          if(err)  throw err;
+            if(result.affectedRows>0){
+                   reslove();
             }else{
                 res.send({code:-2,msg:"系统插入新用户失败,请联系技术人员来进行维护"})
             }
+      })
+    }).then(()=>{
+      pool.query("SELECT * FROM  yn_user WHERE uname=? ",[uname],(err,result)=>{
+        if(err) throw err;
+          if(result.length>0){
+                 res.send({code:1,data:result})
+             }else{
+                 res.send({code:-1,msg:"系统查找失败,请关闭浏览器页面,或者清理浏览器内存,之后在重新注册"})
+            }
          })
-      }
+    })  
+    })
    });
-})
+})();
 
-router.get("/",(req,res)=>{
-  var arr={};
-  pool.query("SELECT * FROM  yn_user",(err,result)=>{
-     if(err) throw err;
-     console.log(result);
-    arr.data=result;  
-      pool.query("SELECT SUM(Cmoney) FROM  yn_user",(err,result)=>{
-          if(err) throw err;
-          arr.total=result;
-           res.send(arr);
-        })
-  })  
-})
+
+
+(function(){
+  router.get("/",(req,res)=>{
+    var arr={};
+    new Promise((reslove,reject)=>{
+    pool.query("SELECT * FROM  yn_user",(err,result)=>{
+       if(err) throw err;
+       else{
+        arr.data=result;  
+        reslove();
+       } 
+    })
+  }).then(function(){
+    pool.query("SELECT SUM(Cmoney) FROM  yn_user",(err,result)=>{
+      if(err) throw err;
+      arr.total=result;
+      res.send(arr);
+    })
+  })
+  })
+})()
+
 
 router.get("/isAll",(req,res)=>{
   let boolean=req.query.boolean;
     if(boolean==="1"){
-      console.log(1)
       pool.query("SELECT * FROM yn_user ORDER BY Cmoney DESC",(err,result)=>{
         if(err) throw err;
         res.send(result)  
      })
     }else{
-      console.log(0)
      pool.query("SELECT * FROM yn_user ORDER BY Cmoney ASC",(err,result)=>{
       if(err) throw err;
       res.send(result)  
    })
     }
-})
+});
 
-
-router.get("/charge",(req,res)=>{
-  var user=JSON.parse(req.query.form);
-  var uname=user.name;
-  var Rdate=user.date;
-  var total=parseInt(user.monty);
-  var auditor=user.auditor;
-  pool.query("SELECT * FROM  yn_user where uname = ?",[uname],(err,result)=>{
-       if(err) throw err;
-       if(result.length>0){
-      
-      var  Cmoney=(parseInt(result[0].Cmoney)+parseInt(total));
-      result[0].Cmoney=Cmoney;
-      var  newResult=result;
-            pool.query("UPDATE yn_user SET Cmoney=? WHERE uname=?",[Cmoney,uname],(err,result)=>{
-                if(err) throw err;
-                if(result.affectedRows>0){
-                        pool.query("INSERT INTO yn_charge VALUES(?,?,?,?,?)",[null,uname,Rdate,total,auditor],(err,result)=>{
-                          if(err) throw err;
-                          if(result.affectedRows>0){
-                            res.send({code:1,data:newResult})  
-                          }else{
-                            res.send({code:-2,msg:"系统充值失败,请联系技术人员来进行维护"})
-                          }
-                        })
-                }else{
-                  res.send({code:-2,msg:"系统更改失败,请联系技术人员来进行维护"})
-                }
-            })
-       }else{
-          res.send({code:-1,msg:"充值用户名不存在,请核实用户名称是否正确"})
-       }
+(function(){
+  router.get("/charge",(req,res)=>{
+    var user=JSON.parse(req.query.form);
+    var uname=user.name;
+    var Rdate=user.date;
+    var total=parseInt(user.monty);
+    var auditor=user.auditor;
+    new Promise((resolve,reject)=>{
+        pool.query("SELECT * FROM  yn_user where uname = ?",[uname],(err,result)=>{
+          if(err) throw err;
+          if(result.length>0){   
+            debugger;
+        var  Cmoney=(parseInt(result[0].Cmoney)+parseInt(total));
+        result[0].Cmoney=Cmoney;
+        var  newResult=result;    
+        var obj = {
+          newResult,
+          Cmoney
+        }
+          resolve(obj);
+          }else{
+            res.send({code:-1,msg:"充值用户名不存在,请核实用户名称是否正确"})
+          }
+        })
+    }).then(function(obj){
+      new Promise((resolve,reject)=>{
+            pool.query("UPDATE yn_user SET Cmoney=? WHERE uname=?",[obj.Cmoney,uname],(err,result)=>{
+              if(err) throw err;
+              if(result.affectedRows>0){
+                  resolve(obj.newResult)
+              }else{
+                res.send({code:-2,msg:"系统更改失败,请联系技术人员来进行维护"})
+              }
+          })
+      }).then(function(newResult){
+        pool.query("INSERT INTO yn_charge VALUES(?,?,?,?,?)",[null,uname,Rdate,total,auditor],(err,result)=>{
+          if(err) throw err;
+          if(result.affectedRows>0){
+            res.send({code:1,data:newResult})  
+          }else{
+            res.send({code:-2,msg:"系统充值失败,请联系技术人员来进行维护"})
+          }
+        })
+      })
+    })
   })
+})();
 
-})
+
+
+
 
 router.get("/update",(req,res)=>{
     var user=JSON.parse(req.query.user);
@@ -146,76 +180,106 @@ router.get("/update",(req,res)=>{
       } 
 
    })
-})
+});
 
-router.get("/history",(req,res)=>{
+(function(){
+
+  router.get("/history",(req,res)=>{
     var user=JSON.parse(req.query.user);
-    
     var uname=user.name; //用户名称
     var Rdate=user.date; //记录日期
     var descInfo=user.desc; //记录详情
     var total=user.money; //消费金额 
     var head=user.business; // 业务人员 
+    new Promise((reslove,reject)=>{
     pool.query("SELECT * FROM  yn_user where uname = ?",[uname],(err,result)=>{
       if(err) throw err;
       if(result.length>0){
         var  Cmoney=(parseInt(result[0].Cmoney)-parseInt(total));
         result[0].Cmoney=Cmoney;
         var newResult=result;
-        pool.query("UPDATE yn_user SET Cmoney=? WHERE uname=?",[Cmoney,uname],(err,result)=>{
-          if(err) throw err;
-             if(result.affectedRows>0){
-                  pool.query("INSERT INTO yn_history VALUES(?,?,?,?,?,?)",[null,uname,Rdate,descInfo,total,head],(err,result)=>{
-                    if(err) throw err;
-                    if(result.affectedRows>0){
-                      res.send({code:1,data:newResult})  
-                    }else{
-                      res.send({code:-2,msg:"系统查询失败,请联系技术人员来进行维护,或者重新登陆"})
-                    }
-                  })
-          }else{
-            res.send({code:-2,msg:"几率更改失败,请联系技术人员来进行维护,或者重新登陆"})
-          }
-         })
+        var obj = {
+          newResult,
+          Cmoney
+        }
+           reslove(obj)
        }else{
            res.send({code:-1,msg:"记录用户名不存在,请核实用户名称是否正确,或者重新登陆"})
        }
 
     })
-   
-})
-router.get("/u",(req,res)=>{
-    var uname=req.query.uname;
-    console.log(uname)
-    pool.query("SELECT * FROM  yn_history WHERE uname=?",[uname],(err,result)=>{
-        if(err) throw err;
-        if(result.length>0){
-           var data=result;
-           pool.query("SELECT SUM(total) FROM  yn_history WHERE uname=?",[uname],(err,result)=>{
-               if(err) throw err;
-              
-              res.send({code:1,data:data,total:result})
-            })
-        }else{
-            res.send({code:-1,msg:"此用户目前没有消费记录"})
-        }
+  }).then(function(obj){
+    new Promise((resolve,reject)=>{
+    pool.query("UPDATE yn_user SET Cmoney=? WHERE uname=?",[obj.Cmoney,uname],(err,result)=>{
+      if(err) throw err;
+         if(result.affectedRows>0){
+          resolve(obj.newResult)
+      }else{
+        res.send({code:-2,msg:"几率更改失败,请联系技术人员来进行维护,或者重新登陆"})
+      }
+     })
+  }).then(function(newResult){
+    pool.query("INSERT INTO yn_history VALUES(?,?,?,?,?,?)",[null,uname,Rdate,descInfo,total,head],(err,result)=>{
+      if(err) throw err;
+      if(result.affectedRows>0){
+        res.send({code:1,data:newResult})  
+      }else{
+        res.send({code:-2,msg:"系统查询失败,请联系技术人员来进行维护,或者重新登陆"})
+      }
     })
+  })    
 })
+});
+})();
 
-router.get("/chargeAll",(req,res)=>{
-  var arr={};
-  pool.query("SELECT * FROM  yn_charge",(err,result)=>{
-     if(err) throw err;
-     console.log(result);
-    arr.data=result;  
-      pool.query("SELECT SUM(total) FROM  yn_charge",(err,result)=>{
+
+(function(){
+  router.get("/u",(req,res)=>{
+    var uname=req.query.uname;
+    new Promise((reslove,reject)=>{
+      pool.query("SELECT * FROM  yn_history WHERE uname=?",[uname],(err,result)=>{ 
+        if(err) throw err;
+          if(result.length>0){
+             reslove(result)
+          }else{
+              res.send({code:-1,msg:"此用户目前没有消费记录"})
+          }
+      })
+    }).then(function(result){
+      var data=result;
+      pool.query("SELECT SUM(total) FROM  yn_history WHERE uname=?",[uname],(err,result)=>{
           if(err) throw err;
-          console.log(result)
-          arr.total=result;
-           res.send(arr);
+          res.send({code:1,data:data,total:result})
         })
-  })  
-})
+    })
+  })
+})();
+
+
+
+
+(function(){
+  router.get("/chargeAll",(req,res)=>{
+    var arr={};
+    new Promise((reslove,reject)=>{
+      pool.query("SELECT * FROM  yn_charge",(err,result)=>{
+        if(err) throw err;
+        else{
+        arr.data=result;
+        reslove(); 
+        } 
+       })
+    }).then(function(){
+          pool.query("SELECT SUM(total) FROM  yn_charge",(err,result)=>{
+            if(err) throw err;
+            arr.total=result;
+            res.send(arr);
+          })
+      })
+    })
+})();
+
+
 
 
 router.get("/All",(req,res)=>{
@@ -226,27 +290,34 @@ router.get("/All",(req,res)=>{
         res.send(result)  
      })
     }else{
-      console.log(0)
      pool.query("SELECT * FROM yn_charge ORDER BY total ASC",(err,result)=>{
       if(err) throw err;
       res.send(result)  
    })
     }
-})
+});
 
-router.get("/c",(req,res)=>{
-  var uname=req.query.uname;
-  pool.query("SELECT * FROM  yn_charge WHERE uname=?",[uname],(err,result)=>{
+
+(function(){
+  router.get("/c",(req,res)=>{
+    var uname=req.query.uname;
+  new Promise((relove,reject)=>{
+      pool.query("SELECT * FROM  yn_charge WHERE uname=?",[uname],(err,result)=>{
+        if(err) throw err;
+        if(result.length>0){
+           relove(result)
+        }else{
+            res.send({code:-1,msg:"此用户目前没有充值记录"})
+        }
+    })
+    }).then(function(result){
+      var data=result;
+      pool.query("SELECT SUM(total) FROM  yn_charge WHERE uname=?",[uname],(err,result)=>{
       if(err) throw err;
-      if(result.length>0){
-         var data=result;
-         pool.query("SELECT SUM(total) FROM  yn_charge WHERE uname=?",[uname],(err,result)=>{
-             if(err) throw err;
-            res.send({code:1,data:data,total:result})
-          })
-      }else{
-          res.send({code:-1,msg:"此用户目前没有充值记录"})
-      }
-  })
-})
+     res.send({code:1,data:data,total:result})
+   })
+    })
+ })
+})();
+
 module.exports=router;
